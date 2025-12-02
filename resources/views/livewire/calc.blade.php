9130c3075d463afec09107f56768ce6ccb1f6d83
kenji-kobayashi15 <kenzicolab.future@gmail.com>
2025-12-02 17:19:18 +0900

    計算アプリ課題完了

--- resources/views/livewire/calc.blade.php
+++ resources/views/livewire/calc.blade.php
@@ -5,37 +5,34 @@ use function Livewire\Volt\{state, mount};
 state(['val1', 'calc', 'val2', 'symbol', 'result']);
 
 mount(function ($val1, $calc, $val2) {
-    $this->val1 = $val1; 
+    $this->val1 = $val1;
     $this->calc = $calc;
     $this->val2 = $val2;
-    $this->symbol = $symbol;
+
+    if ($this->calc === 'addition') {
+        $this->symbol = '+';
+        $this->result = $this->val1 + $this->val2;
+    } elseif ($this->calc === 'subtraction') {
+        $this->symbol = '-';
+        $this->result = $this->val1 - $this->val2;
+    } elseif ($this->calc === 'multiplication') {
+        $this->symbol = '×';
+        $this->result = $this->val1 * $this->val2;
+    } elseif ($this->calc === 'division') {
+        $this->symbol = '÷';
+        if ($this->val2 == 0) {
+            $this->result = '0で割ることはできません';
+        } else {
+            $this->result = $this->val1 / $this->val2;
+        }
+    } else {
+        $this->symbol = ' ? ';
+        $this->result = '無効な演算子です';
+    }
 });
-
-// $calc = addition( + );
-// $calc = subtraction( - );
-// $calc = multiplication( * );
-// $calc = division( / );
-// $calc = ' ' ( ? );
-
-if($this->calc == addition) {
-    $symbol = '+';
-    $result = $val1 + $val2;
-} elseif($this->calc == subtraction) {
-    $symbol = '-';
-    $result = $val1 - $val2;
-} elseif($this->calc == multiplication) {
-    $symbol = '*';
-    $result = $val1 * $val2;
-} elseif($this->calc == division) {
-    $symbol = '/';
-    $result = $val1 / $val2;
-}
-// }else($this->calc == null) {
-//     $symbol = ' ? ';
-//     $result = "無効な演算子です";
 ?>
 
 <div>
     <h1>計算結果</h1>
-    <p>{{ $val1 }} {{ $calc }} {{ $val2 }} = {{ $result }}</p>
+    <p>{{ $val1 }} {{ $symbol }} {{ $val2 }} = {{ $result }}</p>
 </div>
